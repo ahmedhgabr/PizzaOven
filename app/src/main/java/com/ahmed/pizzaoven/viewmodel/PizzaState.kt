76a5afa4824d66e85f756cbbfc293539e05ff490
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-06-11 20:40:31 +0300

    fix: mutableStateList to show changes

--- app/src/main/java/com/ahmed/pizzaoven/viewmodel/PizzaState.kt
+++ app/src/main/java/com/ahmed/pizzaoven/viewmodel/PizzaState.kt
@@ -1,14 +1,18 @@
 package com.ahmed.pizzaoven.viewmodel
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.ahmed.pizzaoven.data.Toppings
 
 
-//data class PizzaState(
-//    val currentIndex: Int = 0,
-//    val pizzaList: MutableList<PizzaInfo> = MutableList(5) { PizzaInfo() },
-//)
-
 data class PizzaState(
+    val currentIndex: Int = 0,
+    val pizzaList: SnapshotStateList<PizzaInfo> = mutableStateListOf(
+        PizzaInfo(), PizzaInfo(), PizzaInfo(), PizzaInfo(), PizzaInfo(),
+    )
+)
+
+data class PizzaInfo(
     val size: Int = 0,
     val toppings: List<Toppings> = emptyList(),
     val price: Int = 17
